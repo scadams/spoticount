59e400aae8b3683faf6b6a4af7b1b25c1a3d4bb1
Sean Adams <adamsseanc@gmail.com>
2024-10-18 06:25:41 -0500

    Improve sorting

--- spoticount.js
+++ spoticount.js
@@ -22,99 +22,104 @@ let fs = require('fs');
 
 // Load Spotify streaming history and sort by timestamp (desc)
 let songHistory = require('./spotify_history.json');
-songHistory.sort(function (a, b) { return new Date(b.ts).getTime() - new Date(a.ts).getTime(); });
+songHistory.sort((a, b) => new Date(b.ts).getTime() - new Date(a.ts).getTime());
 
 
 // map unique songs to play counts
 let songMap = {};
 songHistory.forEach(song => {
-    let songId = song['spotify_track_uri'];
-    if (songId == null) { return; }
-    if (!(songId in songMap)) {
-        songMap[songId] = {
-            artist: song['master_metadata_album_artist_name'],
-            album: song['master_metadata_album_album_name'],
-            track: song['master_metadata_track_name'],
-            spotify_track_uri: songId,
-            song_playcount: 1
-        };
-    } else {
-        songMap[songId].song_playcount += 1;
-    }
+  let songId = song['spotify_track_uri'];
+
+  if (songId == null) { return; }
+
+  if (!(songId in songMap)) {
+    songMap[songId] = {
+      artist: song['master_metadata_album_artist_name'],
+      album: song['master_metadata_album_album_name'],
+      track: song['master_metadata_track_name'],
+      spotify_track_uri: songId,
+      song_playcount: 1
+    };
+  } else {
+    songMap[songId].song_playcount += 1;
+  }
 });
 
-// sort songs by play count (desc)
+// sort songs first by play count (desc), then artist and track (asc)
 let songPlayCounts = Object.values(songMap);
-songPlayCounts.sort(function (a, b) { return b.song_playcount - a.song_playcount; });
+songPlayCounts.sort((a, b) => b.song_playcount - a.song_playcount || a.artist.localeCompare(b.artist) || a.track.localeCompare(b.track));
 
 
 // group songs by artist and map artists to total play counts
 let artistMap = {};
 songPlayCounts.forEach(song => {
-    if (!(song.artist in artistMap)) {
-        artistMap[song.artist] = {
-            artist: song.artist,
-            artist_playcount: song.song_playcount,
-            songs: [song]
-        };
-    } else {
-        artistMap[song.artist].artist_playcount += song.song_playcount,
-        artistMap[song.artist].songs.push(song);
-    }
+  if (!(song.artist in artistMap)) {
+    artistMap[song.artist] = {
+      artist: song.artist,
+      artist_playcount: song.song_playcount,
+      songs: [song]
+    };
+  } else {
+    artistMap[song.artist].artist_playcount += song.song_playcount,
+    artistMap[song.artist].songs.push(song);
+  }
 });
 
-// sort artists by total play count (desc)
-let artistSongPlayCounts = Object.values(artistMap);
-artistSongPlayCounts.sort(function (a, b) { return b.artist_playcount - a.artist_playcount; });
+// sort artists by total play counts (desc)
+let artistSongsPlayCounts = Object.values(artistMap);
+artistSongsPlayCounts.sort((a, b) => b.artist_playcount - a.artist_playcount);
 
 // convert list of songs to the number of songs
-let artistPlayCounts = artistSongPlayCounts.map(({artist, artist_playcount, songs}) => ({artist, artist_playcount, num_songs: songs.length}));
+let artistPlayCounts = artistSongsPlayCounts.map(({artist, artist_playcount, songs}) => ({artist, artist_playcount, num_songs: songs.length}));
 
-// group songs by artist play count
-let artistSongFlatPlayCounts = [];
-artistSongPlayCounts.forEach(artist => {
-    artist.songs.forEach(song => {
-        song.artist_playcount = artist.artist_playcount;
-        artistSongFlatPlayCounts.push(song);
-    });
+// group songs by artist, first sorted by artist play count (desc) then song play count (desc)
+let artistSongsPlayCountsFlat = [];
+artistSongsPlayCounts.forEach(artist => {
+  artist.songs.forEach(song => {
+    song.artist_playcount = artist.artist_playcount;
+    artistSongsPlayCountsFlat.push(song);
+  });
 });
 
 
 function writeJSON(filename, data) {
-    fs.writeFile(filename, JSON.stringify(data), err => { if (err) { console.error(err); } });
+  fs.writeFile(filename, JSON.stringify(data), err => { if (err) { console.error(err); } });
 }
 
 function writeCSV(filename, data) {
-    let csvData = [];
-    csvData.push(Object.keys(data[0]));
-    data.forEach(item => {
-        csvData.push(Object.values(item));
-    });
+  let csvData = [];
+  csvData.push(Object.keys(data[0]));
+  data.forEach(item => {
+    csvData.push(Object.values(item));
+  });
 
-    let csvStr = csvData.map(row =>
-        row
-        .map(String)
-        .map(v => v.replaceAll('"', '""'))
-        .map(v => `"${v}"`)
-        .join(',')
-    ).join('\r\n');
+  let csvStr = csvData.map(row =>
+    row
+      .map(String)
+      .map(v => v.replaceAll('"', '""'))
+      .map(v => `"${v}"`)
+      .join(',')
+  ).join('\r\n');
 
-    fs.writeFile(filename, csvStr, err => { if (err) { console.error(err); } });
+  fs.writeFile(filename, csvStr, err => { if (err) { console.error(err); } });
 }
 
 
 // Create file with streaming history sorted by timestamp (desc)
-// writeJSON(`spotify_history_sorted.json`, songHistory);
+writeJSON(`spotify_history_sorted.json`, songHistory);
+
+
+// Create file with simplified streaming history
+songHistory.map(({}) => {})
 
 
 // Create JSON files
 writeJSON('spotify_playcount_songs.json', songPlayCounts);
 writeJSON('spotify_playcount_artists.json', artistPlayCounts);
-writeJSON('spotify_playcount_artists_songs.json', artistSongPlayCounts);
-writeJSON('spotify_playcount_artists_songs_flat.json', artistSongFlatPlayCounts);
+writeJSON('spotify_playcount_artists_songs.json', artistSongsPlayCounts);
+//writeJSON('spotify_playcount_artists_songs_flat.json', artistSongsPlayCountsFlat);
 
 // Create CSV files
 writeCSV('spotify_playcount_songs.csv', songPlayCounts);
 writeCSV('spotify_playcount_artists.csv', artistPlayCounts);
-writeCSV('spotify_playcount_artists_songs.csv', artistSongFlatPlayCounts);
-
+writeCSV('spotify_playcount_artists_songs.csv', artistSongsPlayCountsFlat);
